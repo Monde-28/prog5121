47c125afa08a15cf1b8778a077e4d14cd4d57bd4
Monde-28 <nomondenene77@gmail.com>
2026-04-12 17:14:15 +0200

    project upload

---
+++ PROG5121_part1/src/main/java/com/mycompany/assignment/Assignment.java
@@ -0,0 +1,8 @@
+package com.mycompany.assignment;
+
+public class Assignment {
+
+    public static void main(String[] args) {
+  registration register = new registration();
+        register.collectInfo();    }
+}
